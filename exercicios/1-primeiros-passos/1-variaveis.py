6ef9d221cb3abcb86d721539cf734dccd2cab2e9
Jonathan Mesquita <81002250+jonathanmesquita@users.noreply.github.com>
2025-04-12 00:07:05 +0000

    Exercicios variaveis_02

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,10 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = 'Jonathan'
 # Declare variáveis de cada tipo numérico
-
+salario = 1234 
 
 # Declare uma variável do tipo booleano
+verdadeiro = True 
+falso = False
